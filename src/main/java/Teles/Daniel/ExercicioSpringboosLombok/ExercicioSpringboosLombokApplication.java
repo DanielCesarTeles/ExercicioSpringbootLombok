f793e6c8481b38ad42c21ca9635c0d98677a0ea8
DanielCesarTeles <daniel.cesarteles@gmail.com>
2020-06-13 01:15:33 -0300

    First commit ExercicioSpringbootLombok

---
+++ src/main/java/Teles/Daniel/ExercicioSpringboosLombok/ExercicioSpringboosLombokApplication.java
@@ -0,0 +1,13 @@
+package Teles.Daniel.ExercicioSpringboosLombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioSpringboosLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioSpringboosLombokApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Teles/Daniel/ExercicioSpringboosLombok/controler/HeloWordController.java
@@ -0,0 +1,14 @@
+package Teles.Daniel.ExercicioSpringboosLombok.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeloWordController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "Página inicial";
+    }
+}
